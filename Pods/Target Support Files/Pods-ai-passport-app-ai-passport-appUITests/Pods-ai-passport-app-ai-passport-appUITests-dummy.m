14896e3d6ccb8d61c3467fc0b982f1e632d23e11
Masaki Nishitani <halotuses@icloud.com>
2025-10-22 00:14:43 +0900

    Remove Pods and DerivedData from tracking

--- Pods/Target Support Files/Pods-ai-passport-app-ai-passport-appUITests/Pods-ai-passport-app-ai-passport-appUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ai_passport_app_ai_passport_appUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ai_passport_app_ai_passport_appUITests
-@end
